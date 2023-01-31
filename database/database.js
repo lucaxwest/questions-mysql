9f5090e748a3937fc18ebdde554d979ae4cc3b92
lucaxwest <lukinhas44_santos@hotmail.com>
2023-01-31 02:22:42 -0300

    no gitignore

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const connection = new Sequelize('guiaperguntas', 'root', 'Aguia@123', {
+const connection = new Sequelize('guiaperguntas', 'root', '************', {
     host: 'localhost',
     dialect: 'mysql'
 });
